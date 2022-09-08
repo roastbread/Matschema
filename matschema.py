2cace3b593fb6906082292ef33be23c365cb9f0a
roastbread <ernil.lunda@gmail.com>
2022-09-08 13:30:32 +0200

    db config

--- matschema.py
+++ matschema.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import sqlite3 as sl
 
-new_meal = input('Skriv in måltid:')
-print(new_meal)
-ingredients = [input('Skriv in ingredienser:')]
-print(ingredients)
+sqliteConnection = sl.connect('matschema.db')
+cursor = sqliteConnection.cursor()
+print('DB Init')
+
+meal_table = """CREATE TABLE MEALTABLE (
+    ID INTEGER NOT NULL,
+    NAME TEXT NOT NULL,
+    PRIMARY KEY (ID,NAME),
+    
+
+
+"""
+
+
+pizza = {'name': 'Pizza', 'ingredients': ['mjöl', 'jäst', 'krossade tomater', 'mozzarella', 'salami', 'svamp']}
+
+
+#new_meal = input('Skriv in måltid:')
+#print(new_meal)
+#ingredients = [input('Skriv in ingredienser:')]
+#print(ingredients)
