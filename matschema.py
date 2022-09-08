428dea440877d59283c760d19586f424d3991ed6
roastbread <97962501+roastbread@users.noreply.github.com>
2022-09-08 16:39:11 +0200

    Update matschema.py

--- matschema.py
+++ matschema.py
@@ -8,13 +8,41 @@ print('DB Init')
 meal_table = """CREATE TABLE MEALTABLE (
     ID INTEGER NOT NULL,
     NAME TEXT NOT NULL,
-    PRIMARY KEY (ID,NAME),
-    
-
-
+    CUISINE TEXT,
+    PRIMARY KEY (ID,NAME)
+);
 """
 
+ingredient_table = """CREATE TABLE INGREDIENTS (
+    ID INTEGER NOT NULL,
+    NAME TEXT NOT NULL,
+    CLASS TEXT NOT NULL,
+    CALORIES REAL,
+    PRICE REAL,
+    PRIMARY KEY (ID,NAME)
+);
+"""
 
+junc_table = """CREATE TABLE MEAL_INGREDIENT_JUNCTION(
+    MealID INTEGER,
+    IngredientID INTEGER,
+    MealNAME TEXT,
+    IngredientNAME TEXT,
+    PRIMARY KEY (MealID, IngredientID, MealNAME, IngredientNAME)
+    FOREIGN KEY (MealID)
+        REFERENCES MEALTABLE (ID),
+    FOREIGN KEY (IngredientID)
+        REFERENCES INGREDIENTS (ID),
+    FOREIGN KEY (MealNAME)
+        REFERENCES MEALTABLE (NAME),
+    FOREIGN KEY (IngredientNAME)
+        REFERENCES INGREDIENTS (NAME)
+);
+"""
+
+cursor.execute(meal_table)
+cursor.execute(ingredient_table)
+cursor.execute(junc_table)
 pizza = {'name': 'Pizza', 'ingredients': ['mjöl', 'jäst', 'krossade tomater', 'mozzarella', 'salami', 'svamp']}
 
 
